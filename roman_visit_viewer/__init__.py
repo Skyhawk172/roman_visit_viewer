5466c6c32121e5d1058ed55550e6c1b7b4171ed4
Charles-Philippe Lajoie <lajoiecp@gmail.com>
2026-03-27 14:58:57 -0400

    Adding demo notebook

--- roman_visit_viewer/__init__.py
+++ roman_visit_viewer/__init__.py
@@ -1,3 +1,13 @@
 # roman_visit/__init__.py
 
+from .roman_visit_viewer import VisitFileParser, plot_manager, main
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "VisitFileParser",
+    "plot_manager",
+    "main",
+]
+
 __version__ = "0.1.0"
